87b09f83edffb9df9564c3dea9085e90c14784c8
useregistrado <oscar.vargas07@uptc.edu.co>
2021-09-09 00:02:04 -0500

    movimiendo de las figuras, arriba, abajo, izquierda y derecha

--- figures/Triangle.py
+++ figures/Triangle.py
@@ -28,6 +28,32 @@ class Triangle:
         pygame.draw.polygon(self.screen, (0, 255, 0), (self.vertex_a.vertex, self.vertex_b.vertex, self.vertex_c.vertex))
 
     @property
+    def move_l(self):
+        self.vertex_a.x = self.vertex_a.x - 10
+        self.vertex_b.x = self.vertex_b.x - 10
+        self.vertex_c.x = self.vertex_c.x - 10
+
+    @property
+    def move_r(self):
+        self.vertex_a.x = self.vertex_a.x + 10
+        self.vertex_b.x = self.vertex_b.x + 10
+        self.vertex_c.x = self.vertex_c.x + 10
+
+    @property
+    def move_u(self):
+        self.vertex_a.y = self.vertex_a.y - 10
+        self.vertex_b.y = self.vertex_b.y - 10
+        self.vertex_c.y = self.vertex_c.y - 10
+
+    @property
+    def move_d(self):
+        self.vertex_a.y = self.vertex_a.y + 10
+        self.vertex_b.y = self.vertex_b.y + 10
+        self.vertex_c.y = self.vertex_c.y + 10
+
+
+
+    @property
     def vertex_a(self):
         return self._vertex_a
 
