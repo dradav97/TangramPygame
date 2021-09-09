a717e8e391bc7eae1dbc9d5344062f7270364275
useregistrado <oscar.vargas07@uptc.edu.co>
2021-09-08 23:41:11 -0500

    se integraron los giros de las figuras

--- prueba_triangulo.py
+++ prueba_triangulo.py
@@ -2,7 +2,7 @@ import sys
 import pygame
 from figures.Triangle import Triangle
 from figures.Vertex import Vertex
-
+from figures.Quadrilateral import Quadrilateral
 
 
 
@@ -19,8 +19,9 @@ alfa = 0
 clock = pygame.time.Clock()
 # (self, height, base, side_ab, side_ac, side_bc, vertex_a, vertex_b, vertex_c, center):
 
-t = Triangle(400, 600, 600, 500, 500, Vertex([100, 500]), Vertex([700, 500]), Vertex([400, 100]), Vertex([400, 350]))
-t2 = Triangle(400, 600, 600, 500, 500, Vertex([100, 500]), Vertex([700, 500]), Vertex([400, 100]), Vertex([400, 350]))
+t = Triangle(Vertex([100, 500]), Vertex([700, 500]), Vertex([400, 100]), Vertex([400, 350]), screen)
+
+c = Quadrilateral(Vertex([10, 30]), Vertex([10, 80]), Vertex([90, 80]), Vertex([90, 30]), Vertex([10, 30]))
 
 while True:
     screen.fill(WHITE)
@@ -32,9 +33,11 @@ while True:
     # pygame.draw.polygon(screen, BLACK, [A, B, C, D], 2)
     # pygame.draw.polygon(screen, (0,255,0), (t.vertex_a.vertex, t.vertex_b.vertex , t.vertex_c.vertex ), 2)
 
-    pygame.draw.polygon(screen, (0, 5, 255), ((50,50),(100,180),(300,285),(20,150)))
-    # g = -0.00172
-    # t2.rotate(g)
+    t.draw()
+    #pygame.draw.polygon((screen), (0, 255, 0), (c.vertex_a.vertex, c.vertex_b.vertex, c.vertex_c.vertex, c.vertex_d.vertex))
+
+    g = -0.00172
+    t.rotate(g)
 
     clock.tick(80)
 
