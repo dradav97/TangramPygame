003d047ad3154a2b688eb42bd2362a23e4cf0658
useregistrado <oscar.vargas07@uptc.edu.co>
2021-10-11 23:09:00 -0500

    added suport to syncronized game

--- menu/Api.py
+++ menu/Api.py
@@ -3,13 +3,21 @@ import requests as rq
 
 class Api:
     def __init__(self):
-        self.url = 'https://c900-186-84-90-148.ngrok.io/status-game'
+        self.url = 'https://c900-186-84-90-148.ngrok.io/'
 
     def get_status(self):
-        response = rq.get(self.url)
+        response = rq.get(self.url+'status-game')
         return response.json()
 
     def set_status(self):
         body = {"status": True, "name": "David"}
-        response = rq.post(self.url, data=body)
+        response = rq.post(self.url+'status-game', data=body)
+        return response.json()
+
+    def set_ready(self):
+        response = rq.post(self.url+'start-player-1')
+        return response.json()
+
+    def all_ready(self):
+        response = rq.get(self.url + 'ready')
         return response.json()
