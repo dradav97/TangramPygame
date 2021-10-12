6ea2568a135115dc8bf9c52e388a4df80d2b6753
useregistrado <oscar.vargas07@uptc.edu.co>
2021-10-12 00:09:43 -0500

    reset automatic

--- menu/Api.py
+++ menu/Api.py
@@ -4,7 +4,7 @@ import requests as rq
 class Api:
     def __init__(self):
         self.url = 'https://c900-186-84-90-148.ngrok.io/'
-
+        self.player = 2
     def get_status(self):
         response = rq.get(self.url+'status-game')
         return response.json()
@@ -15,9 +15,13 @@ class Api:
         return response.json()
 
     def set_ready(self):
-        response = rq.post(self.url+'start-player-1')
+        response = rq.post(self.url+'start-player-'+self.player)
         return response.json()
 
     def all_ready(self):
         response = rq.get(self.url + 'ready')
+        return response.json()
+
+    def reset(self):
+        response = rq.post(self.url + 'reset-player-'+self.player)
         return response.json()
